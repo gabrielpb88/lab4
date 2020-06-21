4ec2d16ee3eb83e924dddac7c66fc24b13fca215
Gabriel Pereira Bastos <gabrielpb88@gmail.com>
2020-06-21 20:59:48 -0300

    Add: JPQL com mais de 1 parametro

--- src/main/java/br/gov/sp/fatec/lab4/dao/PedidoDao.java
+++ src/main/java/br/gov/sp/fatec/lab4/dao/PedidoDao.java
@@ -1,5 +1,6 @@
 package br.gov.sp.fatec.lab4.dao;
 
+import br.gov.sp.fatec.lab4.entities.Cliente;
 import br.gov.sp.fatec.lab4.entities.Pedido;
 
 import javax.persistence.EntityManager;
@@ -11,23 +12,39 @@ public class PedidoDao {
 
     private EntityManager manager = PersistenceManager.getInstance().getEntityManager();
 
-    public Pedido findById(Long id){
+    public Pedido findById(Long id) {
         return manager.find(Pedido.class, id);
     }
 
-    public List<Pedido> findByAttributes(Map<String, String> atributes){
+    public List<Pedido> findPedidosComItemDoCliente(Cliente cliente, String item) {
+        if (cliente == null || item == null || item.trim().length() == 0) return null;
+
+        String queryText = "select p from Pedido p "
+                + "inner join p.items i "
+                + "inner join p.cliente c "
+                + "where i.nome like :nome "
+                + "and c.id = :id";
+
+        TypedQuery<Pedido> typedQuery = manager.createQuery(queryText, Pedido.class);
+        typedQuery.setParameter("nome", item);
+        typedQuery.setParameter("id", cliente.getId());
+
+        return typedQuery.getResultList();
+    }
+
+    public List<Pedido> findByAttributes(Map<String, String> atributes) {
         if (atributes == null) return null;
 
-        String queryText = "select p from Pedido p where ";
+        StringBuilder queryText = new StringBuilder("select p from Pedido p where ");
         int size = atributes.size();
         int i = 0;
-        for(String s : atributes.keySet()){
-            queryText += "p." + s + " like :" + s;
+        for (String s : atributes.keySet()) {
+            queryText.append("p." + s + " like :" + s);
             i++;
-            if(i < size) queryText += " and ";
+            if (i < size) queryText.append(" and ");
         }
-        TypedQuery<Pedido> typedQuery = manager.createQuery(queryText, Pedido.class);
-        for(String s : atributes.keySet()){
+        TypedQuery<Pedido> typedQuery = manager.createQuery(queryText.toString(), Pedido.class);
+        for (String s : atributes.keySet()) {
             typedQuery.setParameter(s, atributes.get(s));
         }
 
